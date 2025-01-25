043880cea731177054571a6c4d0e173a12974c11
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2025-01-25 11:27:26 +0100

    Added more logs and externalized more strings

--- src/main/java/dev/school/app/biblioapp/MainController.java
+++ src/main/java/dev/school/app/biblioapp/MainController.java
@@ -2,10 +2,7 @@ package dev.school.app.biblioapp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -29,10 +26,20 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * Classe controller qui s'occupe de la liaison FXML au Java.
+ */
 public class MainController {
+
+	private static final Logger LOGGER = Main.getLogger();
+	private final ResourceBundle bundle = Main.getBundle();
 
 	private List<Book> books = new ArrayList<>();
 	private File currentFile = null;
@@ -68,8 +75,32 @@ public class MainController {
 	private TableColumn<Book, Boolean> borrowedColumn;
 
 	@FXML
-	private TableColumn<Book, Void> actionColumn;
+	private Label titleLabel;
 
+	@FXML
+	private Label authorLastNameLabel;
+
+	@FXML
+	private Label authorFirstNameLabel;
+
+	@FXML
+	private Label resumeLabel;
+
+	@FXML
+	private Label yearLabel;
+
+	@FXML
+	private Label columnLabel;
+
+	@FXML
+	private Label rowLabel;
+
+	@FXML
+	private Label imageLabel;
+
+	/**
+	 * Fonction principale se lançant lors de l'initialisation du controller.
+	 */
 	@FXML
 	public void initialize() {
 		titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
@@ -92,9 +123,9 @@ public class MainController {
 					setGraphic(null);
 				} else {
 					try {
-						Image image = new Image(pathImage, true); // Asynchronous loading
+						Image image = new Image(pathImage, true);
 						imageView.setImage(image);
-						imageView.setFitWidth(100); // Resize as needed
+						imageView.setFitWidth(100);
 						imageView.setPreserveRatio(true);
 						setGraphic(imageView);
 					} catch (Exception e) {
@@ -103,44 +134,73 @@ public class MainController {
 				}
 			}
 		});
+
+		titleLabel.setText(MessageFormat.format(bundle.getString("order.colon"), bundle.getString("bookTitle")));
+		authorLastNameLabel.setText(MessageFormat.format(bundle.getString("order.colon"), bundle.getString("bookAuthor.lastName")));
+		authorFirstNameLabel.setText(MessageFormat.format(bundle.getString("order.colon"), bundle.getString("bookAuthor.firstName")));
+		resumeLabel.setText(MessageFormat.format(bundle.getString("order.colon"), bundle.getString("bookResume")));
+		yearLabel.setText(MessageFormat.format(bundle.getString("order.colon"), bundle.getString("bookYear")));
+		columnLabel.setText(MessageFormat.format(bundle.getString("order.colon"), bundle.getString("bookColumn")));
+		rowLabel.setText(MessageFormat.format(bundle.getString("order.colon"), bundle.getString("bookRow")));
+		imageLabel.setText(MessageFormat.format(bundle.getString("order.colon"), bundle.getString("bookImage")));
 	}
 
+	/**
+	 * Fonction s'exécutant lorsque l'utilisateur veut fermer l'application. (via le bouton "Quitter")
+	 *
+	 * @param event Évènement / Action utilisateur.
+	 */
 	@FXML
 	void closeApp(ActionEvent event) {
 		Stage stage = (Stage) ((javafx.scene.control.MenuItem) event.getSource())
 				.getParentPopup()
 				.getOwnerWindow();
 
+		LOGGER.info("Closing app...");
 		stage.close();
 	}
 
+	/**
+	 * Fonction s'exécutant lorsque l'utilisateur veut ouvrir un fichier XML contenant des livres. (via le bouton "Ouvrir")
+	 *
+	 * @param event Évènement / Action utilisateur.
+	 */
 	@FXML
 	void openFile(ActionEvent event) {
 		FileChooser fileChooser = new FileChooser();
-		fileChooser.setTitle("Open File");
+		fileChooser.setTitle(bundle.getString("filechooser.open"));
 		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", "*.xml"));
+		LOGGER.info("Opening file...");
 
 		Stage stage = (Stage) ((javafx.scene.control.MenuItem) event.getSource())
 				.getParentPopup()
 				.getOwnerWindow();
 
 		File selectedFile = fileChooser.showOpenDialog(stage);
+		LOGGER.info("Opening file: " + selectedFile);
 
 		if (selectedFile != null) {
-			currentFile = selectedFile; // We update the current file
-			books.clear(); // We clear existing books to avoid duplication
+			currentFile = selectedFile; // On met à jour le fichier actuel
+			books.clear(); // On clear books pour éviter de la duplication
 			try {
-				books.addAll(parseXML(selectedFile)); // We load books from the selected file
-				bookTable.getItems().clear(); // We clear books from the bookTable
-				bookTable.getItems().addAll(books); // We update the bookTable with new loaded books
-				books.forEach(System.out::println);
+				books.addAll(parseXML(selectedFile)); // On load les livres depuis le fichier sélectionné
+				bookTable.getItems().clear(); // On clear books depuis la bookTable pour aussi éviter une dupplication
+				bookTable.getItems().addAll(books); // On update la bookTable avec les nouveaux livres
+				LOGGER.info("Loaded " + books.size() + " books");
+				books.forEach(book -> LOGGER.info("Book loaded: " + book));
 			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println("Failed to load books from file.");
+				LOGGER.log(Level.SEVERE, "Failed to load books from file: " + selectedFile, e);
 			}
+		} else {
+			LOGGER.log(Level.SEVERE, "No file selected");
 		}
 	}
 
+	/**
+	 * Fonction s'exécutant lorsque l'utilisateur veut sauvegarder la bibliothèque. (via le bouton "Sauvegarder")
+	 *
+	 * @param event Évènement / Action utilisateur.
+	 */
 	@FXML
 	void saveFile(ActionEvent event) {
 		if (currentFile != null) {
@@ -150,49 +210,63 @@ public class MainController {
 		}
 	}
 
+	/**
+	 * Fonction s'exécutant lorsque l'utilisateur veut sauvegarder la bibliothèqye dans un nouveau fichier XML. (via le bouton "Sauvegarder sous...")
+	 *
+	 * @param event Évènement / Action utilisateur.
+	 */
 	@FXML
 	void saveAsFile(ActionEvent event) {
 		FileChooser fileChooser = new FileChooser();
-		fileChooser.setTitle("Save As");
+		fileChooser.setTitle(bundle.getString("filechooser.saveAs"));
 		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", "*.xml"));
 
-		// Get the current stage
 		Stage stage = (Stage) ((javafx.scene.control.MenuItem) event.getSource())
 				.getParentPopup()
 				.getOwnerWindow();
 
 		File file = fileChooser.showSaveDialog(stage);
+		LOGGER.info("Saving in file: " + file);
 
 		if (file != null) {
-			currentFile = file; // Update the current file
-			save(file); // Save to the chosen file
+			currentFile = file; // On update le fichier actuel
+			save(file); // On sauvegarde vers le fichier choisi
+		} else {
+			LOGGER.log(Level.SEVERE, "No file selected");
 		}
 	}
 
+	/**
+	 * Fonction s'exécutant lorsque l'utilisateur veut exporter la bibliothèque en un fichier PDF. (via le bouton "Exporter")
+	 *
+	 * @param event Évènement / Action utilisateur.
+	 */
 	@FXML
 	void exportToPDF(ActionEvent event) {
 		if (books.isEmpty()) {
 			Alert alert = new Alert(Alert.AlertType.ERROR);
-			alert.setTitle("Erreur lors de l'export");
-			alert.setHeaderText("Aucun livre chargé !");
-			alert.setContentText("Vous ne pouvez pas exporter un fichier PDF car il n'y a pas de livre chargé dans la bibliothèque.");
+			alert.setTitle(bundle.getString("pdfExport.error.title"));
+			alert.setHeaderText(bundle.getString("pdfExport.error.header"));
+			alert.setContentText(bundle.getString("pdfExport.error.message"));
+			LOGGER.info("Empty book list, cannot export to PDF!");
 			alert.showAndWait();
 			return;
 		}
 
 		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle(bundle.getString("filechooser.exportPDF"));
 		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Files", "*.pdf"));
 
-		// Get the current stage
 		Stage stage = (Stage) ((javafx.scene.control.MenuItem) event.getSource())
 				.getParentPopup()
 				.getOwnerWindow();
 
 		File file = fileChooser.showSaveDialog(stage);
+		LOGGER.info("Exporting to PDF: " + file);
 
 		if (file != null) {
 			try (PDDocument document = new PDDocument()) {
-				// TODO: Title page
+				// TODO: Page de couverture
 				PDPage titlePage = new PDPage(PDRectangle.A4);
 				document.addPage(titlePage);
 				try (var contentStream = new PDPageContentStream(document, titlePage)) {
@@ -203,7 +277,7 @@ public class MainController {
 					contentStream.endText();
 				}
 
-				// TODO: Table of Contents
+				// TODO: Page sommaire avec liens cliquables
 				PDPage tocPage = new PDPage(PDRectangle.A4);
 				document.addPage(tocPage);
 				try (PDPageContentStream contentStream = new PDPageContentStream(document, tocPage)) {
@@ -230,12 +304,11 @@ public class MainController {
 					contentStream.endText();
 				}
 
-				// TODO: Available Books
 				for (Book book : books) {
-					new BookPage(document, book);
+					new BookPage(document, book); // Une page par livre
 				}
 
-				// TODO: Borrowed Books
+				// TODO: Page avec tableau de livres empruntés
 				PDPage borrowedPage = new PDPage(PDRectangle.A4);
 				document.addPage(borrowedPage);
 				try (PDPageContentStream contentStream = new PDPageContentStream(document, borrowedPage)) {
@@ -261,21 +334,31 @@ public class MainController {
 					contentStream.endText();
 				}
 
-				document.save(file); // We save the PDF file
+				document.save(file); // On sauvegarde le document PDF
 			} catch (IOException e) {
-				e.printStackTrace();
+				LOGGER.log(Level.SEVERE, "Failed to export to PDF: " + e.getMessage(), e);
 			}
+		} else {
+			LOGGER.log(Level.SEVERE, "No file selected");
 		}
 	}
 
+	/**
+	 * Fonction permettant de parser un fichier XML en une liste de @Book.
+	 *
+	 * @param file Fichier XML à parser
+	 * @return Une liste de @Book
+	 * @throws Exception Exception retournée si le fichier XML ne contient pas de livres valides.
+	 */
 	private List<Book> parseXML(File file) throws Exception {
 		List<Book> books = new ArrayList<>();
+		LOGGER.info("Parsing XML file: " + file + " into java objects...");
 
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
 		Document document = builder.parse(file);
 
-		document.getDocumentElement().normalize(); // We normalize the XML structure
+		document.getDocumentElement().normalize(); // On normalize la structure XML
 
 		NodeList nodeList = document.getElementsByTagName("livre");
 
@@ -298,14 +381,21 @@ public class MainController {
 				books.add(new Book(title, authorFirstName, authorLastName, description, publicationYear, column, row, pathImage, borrowed));
 			}
 		}
+		LOGGER.info("Parsing complete!");
 		return books;
 	}
 
+	/**
+	 * Fonction permettant de sauvegarder les livres chargés dans la bibliothèque dans un fichier XML.
+	 *
+	 * @param file Le fichier XML de sauvegarde.
+	 */
 	private void save(File file) {
 		try {
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			Document document = builder.newDocument();
+			LOGGER.info("Saving books into XML file: " + file);
 
 			Element root = document.createElement("bibliotheque");
 			document.appendChild(root);
@@ -362,9 +452,9 @@ public class MainController {
 			StreamResult streamResult = new StreamResult(file);
 			transformer.transform(domSource, streamResult);
 
-			System.out.println("Books saved to " + file.getAbsolutePath());
+			LOGGER.info("Books saved to " + file.getAbsolutePath());
 		} catch (Exception e) {
-			e.printStackTrace();
+			LOGGER.log(Level.SEVERE, "There was an issue while saving the books: " + e.getMessage(), e);
 		}
 	}
 }
