2e18dea6540709af685f3661cab707be30b8c3b0
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2025-06-12 16:40:15 +0200

    Fixed some issues, added translations and toolbar

--- src/main/java/dev/school/app/biblioapp/controllers/UserViewController.java
+++ src/main/java/dev/school/app/biblioapp/controllers/UserViewController.java
@@ -1,33 +1,58 @@
 package dev.school.app.biblioapp.controllers;
 
+import dev.school.app.biblioapp.Main;
 import dev.school.app.biblioapp.models.User;
 import dev.school.app.biblioapp.models.UserManager;
+import dev.school.app.biblioapp.views.AboutWindow;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
-public class UserViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
 
-	@FXML
-	private TableView<User> userTable;
-	@FXML
-	private TableColumn<User, String> usernameColumn;
-	@FXML
-	private TableColumn<User, String> passwordColumn;
-	@FXML
-	private TableColumn<User, Boolean> adminColumn;
-	@FXML
-	private TableColumn<User, Void> actionsColumn;
+public class UserViewController implements Initializable {
+
+	private static final Logger LOGGER = Main.getLogger();
+	private final ResourceBundle bundle = Main.getBundle();
 
 	private final ObservableList<User> users = FXCollections.observableArrayList();
 	private final UserManager userManager = new UserManager("src/main/resources/dev/school/app/biblioapp/users.xml");
 
 	@FXML
-	public void initialize() {
+	private Label aboutLabel;
+
+	@FXML
+	private TableView<User> userTable;
+
+	@FXML
+	private TableColumn<User, String> usernameColumn;
+
+	@FXML
+	private TableColumn<User, String> passwordColumn;
+
+	@FXML
+	private TableColumn<User, Boolean> adminColumn;
+
+	@FXML
+	private TableColumn<User, Void> actionsColumn;
+
+	/**
+	 * Fonction principale se lançant lors de l'initialisation du controller.
+	 */
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
 		users.addAll(userManager.getAllUsers());
 		userTable.setItems(users);
+
+		aboutLabel.setOnMouseClicked(e -> new AboutWindow());
 
 		usernameColumn.setCellValueFactory(data ->
 				new javafx.beans.property.SimpleStringProperty(data.getValue().getUsername()));
@@ -47,5 +72,19 @@ public class UserViewController {
 	@FXML
 	private void onSaveUsers() {
 		userManager.saveUsers(users);
+	}
+
+	/**
+	 * Fonction s'exécutant lorsque l'utilisateur veut fermer l'application. (via le bouton "Quitter")
+	 *
+	 * @param event Évènement / Action utilisateur.
+	 */
+	@FXML
+	void closeApp(MouseEvent event) {
+		Stage stage = (Stage) ((javafx.scene.control.Label) event.getSource())
+				.getScene().getWindow();
+
+		LOGGER.info("Closing app...");
+		stage.close();
 	}
 }
