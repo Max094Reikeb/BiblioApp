4a5f2eb6aaccd761283794cce81c0e474f988b0e
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2025-06-17 10:24:23 +0200

    Added missing controller and fixed indentation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,15 @@
 open module dev.school.app.biblioapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.xml;
-    requires java.logging;
-    requires java.desktop;
-    requires org.apache.pdfbox;
-    requires de.jensd.fx.glyphs.fontawesome;
-    requires transitive javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.xml;
+	requires java.logging;
+	requires java.desktop;
+	requires org.apache.pdfbox;
+	requires de.jensd.fx.glyphs.fontawesome;
+	requires transitive javafx.base;
 
-    opens dev.school.app.biblioapp to javafx.fxml;
-    opens dev.school.app.biblioapp.controllers to javafx.fxml;
-
-    exports dev.school.app.biblioapp.controllers;
-    exports dev.school.app.biblioapp.views;
-    exports dev.school.app.biblioapp.models;
-    exports dev.school.app.biblioapp;
+	exports dev.school.app.biblioapp.controllers;
+	exports dev.school.app.biblioapp.views;
+	exports dev.school.app.biblioapp.models;
+	exports dev.school.app.biblioapp;
 }
